0e9b5bf8559601b67c593980ac056b5a7c402bbc
Dmitry <skifcha200211@mail.ru>
2023-07-10 02:44:45 +0500

    Death event handler

--- src/main/java/com/example/Chat/ChatListener.java
+++ src/main/java/com/example/Chat/ChatListener.java
@@ -1,10 +1,14 @@
 package com.example.Chat;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import com.example.DeathCause.DeathCause;
 
 import net.md_5.bungee.api.ChatColor;
 
@@ -18,6 +22,14 @@ public class ChatListener implements Listener {
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         event.setQuitMessage(ChatColor.RED + event.getPlayer().getName() + " вышел");
+    }
+
+    @EventHandler
+    public void onDead(PlayerDeathEvent event) {
+        Player player = event.getEntity();
+        String playerName = player.getName();
+        String deathMessage = DeathCause.getDeathCause(player.getLastDamageCause());
+        event.setDeathMessage(ChatColor.GOLD + playerName + " " + deathMessage);
     }
 
     @EventHandler
