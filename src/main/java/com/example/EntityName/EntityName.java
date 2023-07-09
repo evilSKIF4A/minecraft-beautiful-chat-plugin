718e47f1e49290edd16f089ed9698117c0876b49
Dmitry <skifcha200211@mail.ru>
2023-07-10 02:41:46 +0500

    Add entity name

---
+++ src/main/java/com/example/EntityName/EntityName.java
@@ -0,0 +1,263 @@
+package com.example.EntityName;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class EntityName {
+
+    public static String getEntityName(String entityName) {
+        if (entityName == null)
+            return "";
+        ChatColor colorDarkPurple = ChatColor.DARK_PURPLE;
+
+        switch (entityName) {
+            case "Zombie":
+                entityName = "Зомби";
+                break;
+            case "Zombie Horse":
+                entityName = "Зомби лошадь";
+                break;
+            case "Zombie Villager":
+                entityName = "Зомби житель";
+                break;
+            case "Zombified Piglin":
+                entityName = "Зомби пиглин";
+                break;
+            case "Zoglin":
+                entityName = "Зоглин";
+                break;
+            case "Wolf":
+                entityName = "Волк";
+                break;
+            case "Wither Skeleton":
+                entityName = "Скелет-иссушитель";
+                break;
+            case "Wither":
+                entityName = "Иссушитель";
+                break;
+            case "Witch":
+                entityName = "Ведьма";
+                break;
+            case "Warden":
+                entityName = "Надзератель";
+                break;
+            case "Wandering Trader":
+                entityName = "Бродящий торговец";
+                break;
+            case "Vindicator":
+                entityName = "Защитник";
+                break;
+            case "Villager":
+                entityName = "Сельский житель";
+                break;
+            case "Vex":
+                entityName = "Досаждатель";
+                break;
+            case "Turtle":
+                entityName = "Черепаха";
+                break;
+            case "Tropical Fish":
+                entityName = "Тропическая рыба";
+                break;
+            case "Trader Llama":
+                entityName = "Лама";
+                break;
+            case "Tadpole":
+                entityName = "Головастик";
+                break;
+            case "Strider":
+                entityName = "Страйдер";
+                break;
+            case "Stray":
+                entityName = "Зимогор";
+                break;
+            case "Squid":
+                entityName = "Спрут";
+                break;
+            case "Spider Jockey":
+                entityName = "Скелет-наездник";
+                break;
+            case "Spider":
+                entityName = "Паук";
+                break;
+            case "Sniffer":
+                entityName = "Сниффер";
+                break;
+            case "Snow Golem":
+                entityName = "Снеговик";
+                break;
+            case "Slime":
+                entityName = "Слизь";
+                break;
+            case "Skeleton Horseman":
+                entityName = "Скелет-всадник";
+                break;
+            case "Skeleton Horse":
+                entityName = "Лошадь-скелет";
+                break;
+            case "Skeleton":
+                entityName = "Скелет";
+                break;
+            case "Silverfish":
+                entityName = "Чешуйница";
+                break;
+            case "Shulker":
+                entityName = "Шалкер";
+                break;
+            case "Sheep":
+                entityName = "Овца";
+                break;
+            case "Salmon":
+                entityName = "Лосось";
+                break;
+            case "Ravager":
+                entityName = "Разрушитель";
+                break;
+            case "Rabbit":
+                entityName = "Кролик";
+                break;
+            case "Pufferfish":
+                entityName = "Рыба-фугу";
+                break;
+            case "Polar Bear":
+                entityName = "Полярный медведь";
+                break;
+            case "Pillager":
+                entityName = "Мародёр";
+                break;
+            case "Piglin Brute":
+                entityName = "Брутальный пиглин";
+                break;
+            case "Piglin":
+                entityName = "Пиглин";
+                break;
+            case "Pig":
+                entityName = "Свинья";
+                break;
+            case "Phantom":
+                entityName = "Фантом";
+                break;
+            case "Parrot":
+                entityName = "Попугай";
+                break;
+            case "Panda":
+                entityName = "Панда";
+                break;
+            case "Ocelot":
+                entityName = "Оцелот";
+                break;
+            case "Mule":
+                entityName = "Мул";
+                break;
+            case "Mooshroom":
+                entityName = "Грибная корова";
+                break;
+            case "Magma Cube":
+                entityName = "Лавовый куб";
+                break;
+            case "Llama":
+                entityName = "Лама";
+                break;
+            case "Killer Bunny":
+                entityName = "Кролик-убийца";
+                break;
+            case "Iron Golem":
+                entityName = "Железный голем";
+                break;
+            case "Illusioner":
+                entityName = "Иллюзионист";
+                break;
+            case "Husk":
+                entityName = "Кадавр";
+                break;
+            case "Hoglin":
+                entityName = "Хоглин";
+                break;
+            case "Horse":
+                entityName = "Лошадь";
+                break;
+            case "Guardian":
+                entityName = "Страж";
+                break;
+            case "Goat":
+                entityName = "Коза";
+                break;
+            case "Glow Squid":
+                entityName = "Светящийся спрут";
+                break;
+            case "Giant":
+                entityName = "Зомби-гигант";
+                break;
+            case "Ghast":
+                entityName = "Гаст";
+                break;
+            case "Frog":
+                entityName = "Лягушка";
+                break;
+            case "Fox":
+                entityName = "Лиса";
+                break;
+            case "Evoker":
+                entityName = "Вызыватель";
+                break;
+            case "Endermite":
+                entityName = "Эндермит";
+                break;
+            case "Enderman":
+                entityName = "Эндерман";
+                break;
+            case "Ender Dragon":
+                entityName = "Дракон Эндера";
+                break;
+            case "Elder Guardian":
+                entityName = "Древний страж";
+                break;
+            case "Drowned":
+                entityName = "Утопленник";
+                break;
+            case "Donkey":
+                entityName = "Осел";
+                break;
+            case "Dolphin":
+                entityName = "Дельфин";
+                break;
+            case "Creeper":
+                entityName = "Крипер";
+                break;
+            case "Cow":
+                entityName = "Корова";
+                break;
+            case "Cod":
+                entityName = "Треска";
+                break;
+            case "Chicken Jockey":
+                entityName = "Зомби-наездник";
+                break;
+            case "Chicken":
+                entityName = "Курица";
+                break;
+            case "Cave Spider":
+                entityName = "Пещерный паук";
+                break;
+            case "Cat":
+                entityName = "Кот";
+                break;
+            case "Camel":
+                entityName = "Верблюд";
+                break;
+            case "Blaze":
+                entityName = "Ифрит";
+                break;
+            case "Bee":
+                entityName = "Пчела";
+                break;
+            case "Bat":
+                entityName = "Летучая мышь";
+                break;
+            case "Axolotl":
+                entityName = "Аксолотль";
+                break;
+        }
+
+        return colorDarkPurple + entityName;
+    }
+}
